dfdba268e2655309c1e25a110b9462a3f1fd3dfc
Anton Kuts <anton.a.kuts@gmail.com>
2023-10-20 21:18:42 +0300

    fixed categories img

--- src/UI/Categories/Categories.tsx
+++ src/UI/Categories/Categories.tsx
@@ -11,37 +11,37 @@ const Categories: React.FC = observer(() => {
 			<ul className="categories">
 				<Link to={"/galary/breakfast"} className="categories__card">
 					<div>
-						<img className="categories__card-img" src="./img/breakfast.png" alt="breakfast" />
+						<img className="categories__card-img" src="./images/Categories/breakfast.png" alt="breakfast" />
 						<h3 className="categories__card-title">breakfast</h3>
 					</div>
 				</Link>
 				<Link to={"/galary/main course"} className="categories__card">
 					<div>
-						<img className="categories__card-img" src="src/UI/Categories/img/main course.png" alt="main course" />
+						<img className="categories__card-img" src="./images/Categories/main course.png" alt="main course" />
 						<h3 className="categories__card-title">main course</h3>
 					</div>
 				</Link>
 				<Link to={"/galary/soup"} className="categories__card">
 					<div>
-						<img className="categories__card-img" src="src/UI/Categories/img/soup.png" alt="soup" />
+						<img className="categories__card-img" src="./images/Categories/soup.png" alt="soup" />
 						<h3 className="categories__card-title">soup</h3>
 					</div>
 				</Link>
 				<Link to={"/galary/salad"} className="categories__card">
 					<div>
-						<img className="categories__card-img" src="src/UI/Categories/img/salad.png" alt="salad" />
+						<img className="categories__card-img" src="./images/Categories/salad.png" alt="salad" />
 						<h3 className="categories__card-title">salad</h3>
 					</div>
 				</Link>
 				<Link to={"/galary/appetizer"} className="categories__card">
 					<div>
-						<img className="categories__card-img" src="src/UI/Categories/img/appetizer.png" alt="appetizer" />
+						<img className="categories__card-img" src="./images/Categories/appetizer.png" alt="appetizer" />
 						<h3 className="categories__card-title">appetizer</h3>
 					</div>
 				</Link>
 				<Link to={"/galary/dessert"} className="categories__card">
 					<div>
-						<img className="categories__card-img" src="src/UI/Categories/img/dessert.png" alt="categories__card-img" />
+						<img className="categories__card-img" src="./images/Categories/dessert.png" alt="categories__card-img" />
 						<h3 className="categories__card-title">dessert</h3>
 					</div>
 				</Link>
